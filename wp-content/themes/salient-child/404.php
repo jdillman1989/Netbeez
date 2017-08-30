862a6289874cbb2fffb6015c759d49bec03dd5bc
Jesse Dillman <jesse@jdillman.com>
2017-08-30 14:16:39 -0400

    custom 404 page

---
+++ wp-content/themes/salient-child/404.php
@@ -0,0 +1,33 @@
+<?php 
+get_header(); 
+
+$child_theme = get_stylesheet_directory_uri();
+$error_graphic = file_get_contents($child_theme.'images/404.svg');
+
+?>
+
+<div class="container-wrap">
+	
+	<div class="container main-content">
+		
+		<div class="row">
+			
+			<div class="col span_12">
+				
+				<div id="error-404" class="nb-404-custom">
+					<div class="error-graphic">
+						<?php echo $error_graphic; ?>
+					</div>
+					<h1>404</h1>
+					<h2>Hive not found!</h2>
+				</div>
+				
+			</div><!--/span_12-->
+			
+		</div><!--/row-->
+		
+	</div><!--/container-->
+
+</div>
+<?php get_footer(); ?>
+
